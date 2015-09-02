3c492dfa17bb678c60525965848b7cf3b11c6d4e
John Otander <johnotander@gmail.com>
2015-09-02 10:33:53 -0600

    Begin implementing nested templates

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 var test = require('tape')
+
 var renderPage = require('../lib/render-page')
+var renderTemplate = require('../lib/render-template')
 
 test('page render', function (t) {
   t.plan(1)
@@ -9,4 +11,12 @@ test('page render', function (t) {
   var testPage = fs.readFileSync('test/test-page/index.html', 'utf8')
   var expected = fs.readFileSync('test/test-page/index.expected.html', 'utf8')
   t.equal(testPage, expected)
+})
+
+test('template render', function(t) {
+  t.plan(1)
+
+  var renderedTemplate = renderTemplate('test/templates/nested-template.html')
+  var expected = fs.readFileSync('test/templates/nested-template.expected.html', 'utf8')
+  t.equal(renderedTemplate, expected)
 })
