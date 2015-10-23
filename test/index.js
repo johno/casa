79af1e768a786d29affcc566138040a78545f2af
John Otander <johnotander@gmail.com>
2015-10-22 20:24:39 -0600

    Begin converting tape to ava

--- test/index.js
+++ test/index.js
@@ -1,42 +1,42 @@
-var fs = require('fs')
-var test = require('tape')
+import fs from 'fs'
+import test from 'ava'
 
-var renderPage = require('../lib/render-page')
-var readTemplate = require('../lib/read-template')
-var renderTemplate = require('../lib/render-template')
+import renderPage from '../lib/render-page'
+import readTemplate from '../lib/read-template'
+import renderTemplate from '../lib/render-template'
 
 test('page render', function (t) {
   t.plan(1)
 
   renderPage('test/pages/test-page.html')
-  var testPage = fs.readFileSync('test/test-page/index.html', 'utf8')
-  var expected = fs.readFileSync('test/test-page/index.expected.html', 'utf8')
-  t.equal(testPage, expected)
+  let testPage = fs.readFileSync('test/test-page/index.html', 'utf8')
+  let expected = fs.readFileSync('test/test-page/index.expected.html', 'utf8')
+  t.same(testPage, expected)
 })
 
 test('markdown page render', function (t) {
   t.plan(1)
 
   renderPage('test/pages/foobar.md')
-  var testPage = fs.readFileSync('test/foobar/index.html', 'utf8')
-  var expected = fs.readFileSync('test/foobar/index.expected.html', 'utf8')
-  t.equal(testPage, expected)
+  let testPage = fs.readFileSync('test/foobar/index.html', 'utf8')
+  let expected = fs.readFileSync('test/foobar/index.expected.html', 'utf8')
+  t.same(testPage, expected)
 })
 
 test('template render', function (t) {
   t.plan(1)
 
-  var template = fs.readFileSync('test/templates/nested-template.html', 'utf8')
-  var renderedTemplate = renderTemplate({ content: template })
-  var expected = fs.readFileSync('test/templates/nested-template.expected.html', 'utf8')
-  t.equal(renderedTemplate, expected)
+  let template = fs.readFileSync('test/templates/nested-template.html', 'utf8')
+  let renderedTemplate = renderTemplate({ content: template })
+  let expected = fs.readFileSync('test/templates/nested-template.expected.html', 'utf8')
+  t.same(renderedTemplate, expected)
 })
 
 test('read template', function (t) {
   t.plan(2)
 
-  var expected = '{{{\n  "template": "nested-nested-template",\n  "foobar": "Some string thing"\n}}}\n<div class="awesome-nested-template"></div>\n'
+  let expected = '{{{\n  "template": "nested-nested-template",\n  "foobar": "Some string thing"\n}}}\n<div class="awesome-nested-template"></div>\n'
 
   t.doesNotThrow(readTemplate)
-  t.equal(readTemplate('nested-template'), expected)
+  t.same(readTemplate('nested-template'), expected)
 })
